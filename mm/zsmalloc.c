fc7321d8f4123a566f32a8d4d5e7bf9672670d4c
Sergey Senozhatsky <senozhatsky@chromium.org>
2026-01-07 14:21:44 +0900

    zsmalloc: use actual object size to detect spans
    Using class->size to detect spanning objects is not entirely correct,
    because some size classes can hold a range of object sizes of up to
    class->size bytes in length, due to size-classes merge.  Such classes use
    padding for cases when actually written objects are smaller than
    class->size.  zs_obj_read_begin() can incorrectly hit the slow path and
    perform memcpy of such objects, basically copying padding bytes.  Instead
    of class->size zs_obj_read_begin() should use the actual compressed object
    length (both zram and zswap know it) so that it can correctly handle
    situations when a written object is small enough to fit into the first
    physical page.
    
    Link: https://lkml.kernel.org/r/20260107052145.3586917-1-senozhatsky@chromium.org
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Reviewed-by: Yosry Ahmed <yosry.ahmed@linux.dev>	[zsmalloc & zswap]
    Reviewed-by: Nhat Pham <nphamcs@gmail.com>
    Cc: Brian Geffon <bgeffon@google.com>
    Cc: Chengming Zhou <chengming.zhou@linux.dev>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: iDeadXS <datarafi43@gmail.com>
    
    # Conflicts:
    #	drivers/block/zram/zram_drv.c
    #	mm/zswap.c

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -1131,7 +1131,7 @@ unsigned long zs_get_total_pages(struct zs_pool *pool)
 EXPORT_SYMBOL_GPL(zs_get_total_pages);
 
 void *zs_obj_read_begin(struct zs_pool *pool, unsigned long handle,
-			void *local_copy)
+			size_t mem_len, void *local_copy)
 {
 	struct zspage *zspage;
 	struct zpdesc *zpdesc;
@@ -1153,7 +1153,10 @@ void *zs_obj_read_begin(struct zs_pool *pool, unsigned long handle,
 	class = zspage_class(pool, zspage);
 	off = offset_in_page(class->size * obj_idx);
 
-	if (off + class->size <= PAGE_SIZE) {
+	if (!ZsHugePage(zspage))
+		mem_len += ZS_HANDLE_SIZE;
+
+	if (off + mem_len <= PAGE_SIZE) {
 		/* this object is contained entirely within a page */
 		addr = kmap_local_zpdesc(zpdesc);
 		addr += off;
@@ -1162,7 +1165,7 @@ void *zs_obj_read_begin(struct zs_pool *pool, unsigned long handle,
 
 		/* this object spans two pages */
 		sizes[0] = PAGE_SIZE - off;
-		sizes[1] = class->size - sizes[0];
+		sizes[1] = mem_len - sizes[0];
 		addr = local_copy;
 
 		memcpy_from_page(addr, zpdesc_page(zpdesc),
@@ -1181,7 +1184,7 @@ void *zs_obj_read_begin(struct zs_pool *pool, unsigned long handle,
 EXPORT_SYMBOL_GPL(zs_obj_read_begin);
 
 void zs_obj_read_end(struct zs_pool *pool, unsigned long handle,
-		     void *handle_mem)
+		     size_t mem_len, void *handle_mem)
 {
 	struct zspage *zspage;
 	struct zpdesc *zpdesc;
@@ -1195,7 +1198,10 @@ void zs_obj_read_end(struct zs_pool *pool, unsigned long handle,
 	class = zspage_class(pool, zspage);
 	off = offset_in_page(class->size * obj_idx);
 
-	if (off + class->size <= PAGE_SIZE) {
+	if (!ZsHugePage(zspage))
+		mem_len += ZS_HANDLE_SIZE;
+
+	if (off + mem_len <= PAGE_SIZE) {
 		if (!ZsHugePage(zspage))
 			off += ZS_HANDLE_SIZE;
 		handle_mem -= off;
