8745537729eb3a438e76b5d79d84b207ce9aec12
iDeadXS <datarafi43@gmail.com>
2026-09-14 19:23:35 +0700

    zsmalloc: checkout to android15-6.6-desktop
    Signed-off-by: iDeadXS <datarafi43@gmail.com>

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -120,6 +120,8 @@
 #define ISOLATED_BITS	5
 #define MAGIC_VAL_BITS	8
 
+#define MAX(a, b) ((a) >= (b) ? (a) : (b))
+
 #define ZS_MAX_PAGES_PER_ZSPAGE	(_AC(CONFIG_ZSMALLOC_CHAIN_SIZE, UL))
 
 /* ZS_MIN_ALLOC_SIZE must be multiple of ZS_ALIGN */
@@ -986,9 +988,6 @@ static struct zspage *alloc_zspage(struct zs_pool *pool,
 
 	if (!zspage)
 		return NULL;
-
-	if (!IS_ENABLED(CONFIG_COMPACTION))
-		gfp &= ~__GFP_MOVABLE;
 
 	zspage->magic = ZSPAGE_MAGIC;
 	migrate_lock_init(zspage);
